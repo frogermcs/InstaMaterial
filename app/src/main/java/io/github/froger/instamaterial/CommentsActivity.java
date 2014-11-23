1e4d77629994c95ad3c0d268fc2a8c4af71a677b
froger_mcs <froger.mcs@gmail.com>
2014-11-23 16:48:08 +0100

    added Picasso dependency, Comments Acitivity initialization,

---
+++ app/src/main/java/io/github/froger/instamaterial/CommentsActivity.java
@@ -0,0 +1,33 @@
+package io.github.froger.instamaterial;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.widget.LinearLayout;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+/**
+ * Created by froger_mcs on 11.11.14.
+ */
+public class CommentsActivity extends ActionBarActivity {
+    public static final String ARG_DRAWING_START_LOCATION = "arg_drawing_start_location";
+
+    @InjectView(R.id.toolbar)
+    Toolbar toolbar;
+    @InjectView(R.id.contentRoot)
+    LinearLayout contentRoot;
+    @InjectView(R.id.rvComments)
+    RecyclerView rvComments;
+    @InjectView(R.id.llAddComment)
+    LinearLayout llAddComment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comments);
+        ButterKnife.inject(this);
+    }
+}
