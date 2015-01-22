44b977232ac98d4e9c1f2dee91077f9b11705d43
Mirek Stanek <froger.mcs@gmail.com>
2015-01-22 22:36:45 +0100

    Add circle transformation for Picasso

---
+++ app/src/main/java/io/github/froger/instamaterial/CircleTransformation.java
@@ -0,0 +1,56 @@
+package io.github.froger.instamaterial;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+import com.squareup.picasso.Transformation;
+
+/**
+ * Created by Miroslaw Stanek on 20.01.15.
+ */
+public class CircleTransformation implements Transformation {
+
+    private static final int STROKE_WIDTH = 6;
+
+    @Override
+    public Bitmap transform(Bitmap source) {
+        int size = Math.min(source.getWidth(), source.getHeight());
+
+        int x = (source.getWidth() - size) / 2;
+        int y = (source.getHeight() - size) / 2;
+
+        Bitmap squaredBitmap = Bitmap.createBitmap(source, x, y, size, size);
+        if (squaredBitmap != source) {
+            source.recycle();
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(size, size, source.getConfig());
+
+        Canvas canvas = new Canvas(bitmap);
+
+        Paint avatarPaint = new Paint();
+        BitmapShader shader = new BitmapShader(squaredBitmap, BitmapShader.TileMode.CLAMP, BitmapShader.TileMode.CLAMP);
+        avatarPaint.setShader(shader);
+
+        Paint outlinePaint = new Paint();
+        outlinePaint.setColor(Color.WHITE);
+        outlinePaint.setStyle(Paint.Style.STROKE);
+        outlinePaint.setStrokeWidth(STROKE_WIDTH);
+        outlinePaint.setAntiAlias(true);
+
+        float r = size / 2f;
+        canvas.drawCircle(r, r, r, avatarPaint);
+        canvas.drawCircle(r, r, r - STROKE_WIDTH / 2, outlinePaint);
+
+        squaredBitmap.recycle();
+        return bitmap;
+    }
+
+    @Override
+    public String key() {
+        return "circleTransformation()";
+    }
+}
